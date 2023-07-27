0c25fe3b84d2fa08eb44e899f58d14adaa25db5f
Sai Ganesh <105867763+saigani219@users.noreply.github.com>
2023-07-27 21:17:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let lists = document.getElementsByTagName("li");
+for (let index = 0; index < lists.length; index++) {
+	if(lists[index].id === "level"){
+		alert("The level of the element is: " + (index + 1));
+	}
+}
